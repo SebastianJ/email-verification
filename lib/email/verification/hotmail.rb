f7243ad02e532ffc2943798a122470c928f430b5
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-07-04 10:10:14 +0200

    Bug fix for Hotmail email verifier

--- lib/email/verification/hotmail.rb
+++ lib/email/verification/hotmail.rb
@@ -2,8 +2,8 @@ module Email
   module Verification
     class Hotmail < Base
       
-      def retrieve_verification_code(email:, password:, settings: {})
-        super(email: email, password: password, host: "outlook.office365.com", port: 993, enable_ssl: true, mailboxes: %w(Inbox Junk), settings: settings)
+      def retrieve_verification_code(email:, password:, mailboxes: %w(Inbox Junk), settings: {})
+        super(email: email, password: password, host: "outlook.office365.com", port: 993, enable_ssl: true, mailboxes: mailboxes, settings: settings)
       end
       
     end
--- lib/email/verification/version.rb
+++ lib/email/verification/version.rb
@@ -1,5 +1,5 @@
 module Email
   module Verification
-    VERSION = "0.1.4"
+    VERSION = "0.1.5"
   end
 end
